7b18a8a6b87f08866d5f3494c6d1f47cefca1fc6
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2022-06-27 17:18:47 -0400

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mt-3 ml-3 font-extrabold tracking-tight text-slate-900">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
