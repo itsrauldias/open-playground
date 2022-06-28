59158e4022a733d3c5b3c42e58bdda40e7f9f5c5
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2022-06-28 16:39:21 -0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="mt-3 ml-3 font-extrabold tracking-tight text-slate-900">Hello Tailwind!</h1>
+      <h3>Open playground</h3>
     </div>
   );
 }
