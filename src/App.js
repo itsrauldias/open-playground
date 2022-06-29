46af190e9a2ca8b79cd563b9511f8fad5c75f01a
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2022-06-29 08:01:06 -0400

    220629-01

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import QrCodeGenerator from './Components/QrCodeGenerator';
+import logo from './favicon.png'
 
 function App() {
 
-  const [qrCode] = useState("https://github.com/itsrauldias")
+  const [qrCode] = useState('https://github.com/itsrauldias')
 
   return (
     <div className="App">
       <h3>Open playground</h3>
       <hr />
       <div style={{ textAlign: 'center' }}>
-        <p>{qrCode}</p>
-        <QrCodeGenerator text={qrCode} />
+        <h3>{qrCode}</h3>
+        <QrCodeGenerator
+          text={qrCode}
+          // width={100}
+          // height={100}
+          colorDark='black'
+          logo={logo}
+        />
       </div>
     </div>
   );
--- src/Components/QrCodeGenerator/index.jsx
+++ src/Components/QrCodeGenerator/index.jsx
@@ -3,25 +3,30 @@ import * as QRCode from 'easyqrcodejs';
 
 class QrCodeGenerator extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.qrcode = React.createRef();
+  constructor(props) {
+    super(props);
+    this.qrcode = React.createRef();
+  }
+
+  componentDidMount() {
+    // Options
+    var options = {
+      text: `${this.props.text}`,
+      width: this.props.width || 256,
+      height: this.props.height || 256,
+      colorDark: this.props.colorDark || '#000000',
+      logo: this.props.logo || false,
+      drawer: 'svg'
     }
 
-    componentDidMount() {
-        // Options
-        var options = {
-            text: `${this.props.text}`
-        }
-
-        // Create new QRCode Object
-        new QRCode(this.qrcode.current, options);
-    }
-    render() {
-        return (
-            <div ref={this.qrcode} />
-        );
-    }
+    // Create new QRCode Object
+    new QRCode(this.qrcode.current, options);
+  }
+  render() {
+    return (
+      <div ref={this.qrcode} />
+    );
+  }
 }
 
 export default QrCodeGenerator;
